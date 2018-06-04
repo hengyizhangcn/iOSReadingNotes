808c8ce19553cf9b66f1c5cda802ba88b25de081
hengyizhangcn <hengyizhangcn@gmail.com>
2018-06-04 10:39:35 +0800

    new content

---
+++ ProjectDemo/NSObjectDemo/NSObjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSObjectDemo
+//
+//  Created by zhy on 2018/6/4.
+//  Copyright © 2018 zhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectDemo/NSObjectDemo/NSObjectDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSObjectDemo
+//
+//  Created by zhy on 2018/6/4.
+//  Copyright © 2018 zhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
